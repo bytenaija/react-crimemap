536596e014757a44897a4e0ac6f9618a0ca634d7
Everistus Olumese <everistusemail@gmail.com>
2019-10-19 17:47:09 +0100

    Deploying to heroku

--- src/views/MapView/MapView.js
+++ src/views/MapView/MapView.js
@@ -5,13 +5,14 @@ import { useSelector, useDispatch } from "react-redux";
 import Types from "../../store/crimes/types";
 
 export const MapView = () => {
+  const BASE_URL = process.env.NODE_ENV === 'production'? 'https://crimemap-apiv2.herokuapp.com/api' : 'http://localhost:5009/api'
   const crimes = useSelector(state => state.Crimes.crimes);
   const dangerAlertCrimes = useSelector(state => state.Crimes.dangerAlertCrimes);
   const dispatch = useDispatch();
   const { latitude, longitude } = usePosition();
 
   useEffect(() => {
-    const socket = socketIOClient("http://localhost:5009");
+    const socket = socketIOClient(BASE_URL);
     socket.emit("update-location", {
       userId: "5da82087c3667279b8ff98c7",
       longitude: longitude,
